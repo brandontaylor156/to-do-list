a27764f3f666790fe408c3b31ee119f8cbf210ac
brandontaylor156 <brandon.david4@outlook.com>
2022-08-31 21:22:37 -0700

    to do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Task from './components/Task'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  
+
+  const handleNewTask = (event) => {
+      event.preventDefault();
+
+      if (newTask.length === 0){
+        return;
+      }
+
+      const taskItem = {
+        text: newTask,
+        complete: false
+      }
+
+      setTasks([...tasks, taskItem])
+      setNewTask("")
+  }
+
+  const handleDelete = (index) => {
+    const filteredTasks = tasks.filter((todo, i) => {
+      return i !== index;
+    });
+
+    setTasks(filteredTasks)
+  }
+
+  const handleToggleComplete = (index) => {
+    const updatedTasks = tasks.map((task, i) => {
+        if (index === i){
+          task.complete = !task.complete;
+        }
+        return task;
+    });    
+    setTasks(updatedTasks)
+  }
+    
+    return (
+        <div>
+            <form onSubmit={(event) => {
+              handleNewTask(event);
+              }}>
+              <input onChange={(event) => {
+                setNewTask(event.target.value)
+              }} type="text" value={newTask}
+              />
+              <div>
+                <button>Add</button>
+              </div>
+            </form>
+
+           
+            {
+              tasks.map((task, index) =>  {
+
+              const taskStyles = [];
+              if (task.complete){
+                taskStyles.push("strike-through")
+              }
+
+                return (
+                  <Task key={index} task={task} handleToggleComplete={handleToggleComplete} index={index}
+                    handleDelete={handleDelete} taskStyles={taskStyles}/>
+               )})
+            }
+        </div>
+    );
 }
+    
+//export default App;
 
-export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,16 @@
+const Task = (props) => {
+    return (
+    <div>
+        <input onChange={(event) =>{
+            props.handleToggleComplete(props.index);
+            }} type="checkbox" checked={props.task.complete}/>
+        <span className={props.taskStyles.join(" ")}>{props.task.text}</span>
+
+        <button onClick= {(event) => {
+            props.handleDelete(props.index);
+            }}>Delete</button>
+    </div>
+    )
+}
+
+export default Task
